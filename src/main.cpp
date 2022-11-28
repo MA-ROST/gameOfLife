d3597a510f242f88c6b6cf07be4c49859ea53ab7
Marissa R.S <mrowles@academic.rrc.ca>
2022-11-28 10:17:40 -0600

    Setup Canvas
    Removed resize, set size, added title

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,20 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	const string title               = "Game Of Life";
+	constexpr Point <int> screenSize = {600, 600};
+
+	ofGLFWWindowSettings settings;
+	settings.setSize(screenSize.x, screenSize.y);
+	settings.title = title;
+	settings.resizable = false;
+	ofCreateWindow(settings);
+	ofSetFrameRate(24);
+	ofSetWindowTitle(title); // Set the window title
+
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
-
 }
