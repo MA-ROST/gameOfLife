99804857f3ca99087f3c84c539dbda382b21ba61
Marissa R.S <mrowles@academic.rrc.ca>
2022-11-28 11:05:33 -0600

    Added cell classes
    these are adapted from an old project from highschool, as well as my pixel batman project

--- src/cell.cpp
+++ src/cell.cpp
@@ -1,1 +1,33 @@
 ﻿#include "cell.h"
+
+void Cell::setupPixel(const int& x, const int& y, bool isFilled)
+{
+	pixelSize_ = calculatePixelSize();
+	setStyle(isFilled);
+	ofDrawRectangle(pixelSize_.x * x, pixelSize_.y * y, pixelSize_.x, pixelSize_.y);
+}
+
+void Cell::setStyle(bool state) const
+{
+	ofFill();
+	switch (state) {
+	case true:
+		ofSetColor(active);
+		break;
+	default:
+		ofNoFill();
+		ofSetColor(inactive);
+		break;
+	}
+
+}
+
+Point<float> Cell::calculatePixelSize()
+{
+	Point <float> output{
+		static_cast <float> (ofGetWidth()) / GRID_SIZE.x,
+		static_cast <float> (ofGetHeight()) / GRID_SIZE.y
+	};
+
+	return output;
+}
--- src/cell.h
+++ src/cell.h
@@ -1,7 +1,42 @@
 ﻿#pragma once
 
+#include "ofMain.h"
+#include "point.h"
+
 class Cell {
 public:
 	bool isLive_;
+	Point<float> pixelSize_, pixelLocation_;
+
+	enum stateColor {
+		inactive = 150,
+		active = 0
+	};
+	static constexpr Point<int> GRID_SIZE{ 24,24 };
+
+	/**
+	 * @brief sets up the pixel for drawing, and then draws it
+	 *
+	 * @param x The x index of the vector
+	 * @param y The y index of the vector
+	 * @param isFilled determines the style of the pixel
+	 */
+	void setupPixel(const int& x, const int& y, bool isFilled);
+
+private:
+
+	/**
+	 * @brief Determines the visual look of a pixel based on a given number
+	 * @param state changes the color of a pixel between stateColors
+	 */
+	void setStyle(bool state) const;
+
+	/**
+	 * @brief through the amount of pixels and size of the window,
+	 * the height and width of a pixel is determined
+	 *
+	 * @return The calculated height and width for each pixel
+	 */
+	static Point<float> calculatePixelSize();
 	
 };
